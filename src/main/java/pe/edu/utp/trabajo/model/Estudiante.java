2311eddd0dae33eeaebc5fd0c710e3440593b01e
Aaron-Yiru <josechuctaya.1fenix@gmail.com>
2026-04-06 09:01:32 -0500

    Iniciando proyecto

---
+++ src/main/java/pe/edu/utp/trabajo/model/Estudiante.java
@@ -0,0 +1,17 @@
+package pe.edu.utp.trabajo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Estudiante {
+    private int id;
+    private String nombre;
+    private String email;
+
+}
---
+++ src/test/java/pe/edu/utp/trabajo/TrabajoApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.utp.trabajo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabajoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
